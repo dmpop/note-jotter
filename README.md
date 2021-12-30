f7e5408c819882995ff1f19b772424c037523979
Dmitri Popov <dmpop@linux.com>
2021-12-30 21:27:15 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Scratch That
+
+Scratch that is a minimalist note-taking tool written in PHP. Scratch that stores content is in a plain text file. The editor create a backup copy of the text file on each save.
+
+## Dependencies
+
+- PHP 7.x or higher
+
+# Installation and Usage
+
+To run Scratch That locally, run the `php -S localhost:8000` command and point your browser to _localhost:8000*_
+
+To deploy Scratch that on a web server, move the *scratch-that* directory to the document root of the server. Point your browser to *127.0.0.1/scratch-that* (replace *127.0.0.1* with the actual IP address or domain name of your server).
---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+error_reporting(E_ERROR);
+$title = "Scratch that";
+$password = 'password';
+// Content directory
+$dir = 'content';
+$theme = "light";
+$footer = "I really 🧡 <a href='https://www.paypal.com/paypalme/dmpop'>coffee</a>";
+?>
+
+<!DOCTYPE html>
+<html lang="en" data-theme="<?php echo $theme; ?>">
+
+<!-- Author: Dmitri Popov, dmpop@linux.com
+	 License: GPLv3 https://www.gnu.org/licenses/gpl-3.0.txt -->
+
+<head>
+	<title><?php echo $title; ?></title>
+	<meta charset="utf-8">
+	<link rel="shortcut icon" href="favicon.png" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/classless.css" />
+	<link rel="stylesheet" href="css/themes.css" />
+	<style>
+		textarea {
+			font-size: 15px;
+			width: 100%;
+			height: 15em;
+			line-height: 1.9;
+			margin-top: 2em;
+		}
+	</style>
+	<!-- Suppress form re-submit prompt on refresh -->
+	<script>
+		if (window.history.replaceState) {
+			window.history.replaceState(null, null, window.location.href);
+		}
+	</script>
+</head>
+
+<body>
+	<div class="card text-center">
+		<div style="margin-top: 1em; margin-bottom: 1em;">
+			<img style="display: inline; height: 2.5em; vertical-align: middle;" src="favicon.svg" alt="logo" />
+			<h1 style="display: inline; margin-top: 0em; vertical-align: middle; letter-spacing: 3px;"><?php echo $title; ?></h1>
+		</div>
+		<hr>
+		<?php
+		if (!file_exists($dir)) {
+			mkdir($dir, 0777, true);
+		}
+		$mdfile = $dir . "/content.md";
+		function Read()
+		{
+			include('config.php');
+			$mdfile = $dir . "/content.md";
+			echo file_get_contents($mdfile);
+		}
+		function Write()
+		{
+			include('config.php');
+			$mdfile = $dir . "/content.md";
+			copy($mdfile, $dir . '/' . date('Y-m-d-H-i-s') . '.md');
+			$fp = fopen($mdfile, "w");
+			$data = $_POST["text"];
+			fwrite($fp, $data);
+			fclose($fp);
+		}
+		?>
+		<?php
+		if (isset($_POST["save"])) {
+			if ($_POST['password'] != $password) {
+				print '<p>Wrong password</p>';
+				exit();
+			}
+			Write();
+			header('Location:index.php');
+		};
+		?>
+		<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+			<textarea name="text"><?php Read(); ?></textarea>
+			<input style="margin-top: 1em;" type="password" name="password">
+			<button style="margin-top: .5em; margin-bottom: 1em;" type="submit" name="save">Save</button>
+		</form>
+	</div>
+	<div class="text-center">
+		<?php echo $footer; ?>
+	</div>
+</body>
+
+</html>
